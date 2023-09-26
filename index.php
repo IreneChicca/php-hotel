13661723329a2f25ccb0c1c03eff761adccddc5c
IreneChicca <irene.chicca@outlook.com>
2023-09-26 15:40:47 +0200

    Print hotel in table

--- index.php
+++ index.php
@@ -48,18 +48,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <?php
+    
+
+
+
+<table class="table text-center">
+  <thead>
+    <tr>
+
+    <?php 
+    foreach ($hotels[0] as $key => $hotel) {
+
+            echo "<th scope='col'>".$key."</th>";
+      
+    }
+    ?>          
+      
+    </tr>
+  </thead>
+  <tbody>
+<?php
     
         foreach ($hotels as  $hotel) {
-          foreach ($hotel as $element)
+       echo  "<tr>
+       <td>".$hotel['name']."</td>
+      <td>".$hotel['description']."</td>
+
+      <td>". 
+      
+      //if($hotel['parking']) ? 'yes' : 'no'  
+
+      $hotel['parking'].
+      "</td>
+
+      <td>".$hotel['vote']."</td>
+      <td>".$hotel['distance_to_center']."</td>"
+    ;
+
+          foreach ($hotel as $element) 
                 { 
-                    echo "$element <br>";
+                    // echo $hotel['name'];
                 }
 
         };
     
     ?>
+
+
+    
+  </tbody>
+</table>
 </body>
 </html>
